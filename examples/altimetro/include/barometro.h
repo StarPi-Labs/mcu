bffb7598ddab52a4710a37b44b916ea809518335
mcsio <m.causio@studenti.unipi.it>
2026-03-01 11:57:28 +0100

    Altimetro + filtro passa basso

---
+++ examples/altimetro/include/barometro.h
@@ -0,0 +1,25 @@
+#ifndef BAROMETRO_H
+#define BAROMETRO_H
+
+#include <Arduino.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/queue.h>
+
+// Struttura che contiene tutti i dati del volo letti dal barometro
+struct DatiBarometro {
+    uint32_t timestamp;
+    float temperatura;
+    float pressione;
+    float altitudine;
+    int status_errore;
+};
+
+// Handle della coda: gli altri task useranno questa per leggere i tuoi dati
+extern QueueHandle_t codaBarometro;
+
+// Funzioni pubbliche
+bool initBarometro();
+void vTaskBarometro(void *pvParameters);
+
+#endif

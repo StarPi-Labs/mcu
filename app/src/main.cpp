b8b82a294e763678b8613daa573542b88939bb48
AimanDebug <105602441+AimanDebug@users.noreply.github.com>
2026-03-18 21:57:34 +0100

    Rifattorizzato codice del main

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,23 +1,37 @@
 #include "barometro.h"
 #include <Arduino.h>
 
+namespace mcu {
+    bool init();
+};
 
 void setup() {
-    Serial.begin(115200);// da settare baudrate del pc uguale
-    Serial.println("Avvio Sistema Avionico...");
-    
-    // inizializzo modulo
-    if (initBarometro()) {
-        // Se l'init va a buon fine, avvia il task. assegno 2048 byte, con priorità 3 (alta)
-        xTaskCreate(vTaskBarometro, "TaskBaro", 2048, NULL, 3, NULL);
-        
-        // Avvia il task del team (Logger)
-        xTaskCreate(vTaskDatalogger, "TaskLog", 2048, NULL, 2, NULL);
-    } else {
-        Serial.println("Avvio interrotto causa guasto sensore.");
-    }
+  Serial.begin(115200); // da settare baudrate del pc uguale
+  Serial.println("[Info]: Avvio Sistema Avionico...");
+
+  if (!mcu::init()) {
+    Serial.print("[Errore]: Inizializzazione fallita");
+    return;
+  }
+
+  Serial.println("[Info]: Inizialiazzazione completata con successo");
+}
+
+bool mcu::init() {
+  if (!initBarometro()) {
+    Serial.println("Avvio interrotto causa guasto sensore.");
+    return false;
+  }
+
+  // Se l'init va a buon fine, avvia il task. assegno 2048 byte, con priorità 3
+  // (alta)
+  xTaskCreate(vTaskBarometro, "TaskBaro", 2048, NULL, 3, NULL);
+  // Avvia il task del team (Logger)
+  xTaskCreate(vTaskDatalogger, "TaskLog", 2048, NULL, 2, NULL);
+
+  return true;
 }
 
 void loop() {
-    // Vuoto in FreeRTOS
+  // Vuoto in FreeRTOS
 }

bf79f13e5f7e41c715abd7696d58c8427fd854f1
Enrico Lumetti <e.lumetti@studenti.unipi.it>
2026-04-30 22:29:34 +0200

    output angles as deg, fix step() order

--- examples/logger/simulink/PlaneNavigationFilter_ert_rtw/rt_nonfinite.c
+++ examples/logger/simulink/PlaneNavigationFilter_ert_rtw/rt_nonfinite.c
@@ -7,9 +7,9 @@
  *
  * Code generated for Simulink model 'PlaneNavigationFilter'.
  *
- * Model version                  : 1.101
+ * Model version                  : 1.102
  * Simulink Coder version         : 24.2 (R2024b) 21-Jun-2024
- * C/C++ source code generated on : Thu Apr 30 20:32:16 2026
+ * C/C++ source code generated on : Thu Apr 30 22:23:55 2026
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: Custom Processor->Custom Processor

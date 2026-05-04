3bb4969705a18be552c33151deb6e81b3efb8e45
AimanDebug <105602441+AimanDebug@users.noreply.github.com>
2026-05-04 19:00:16 +0200

    feat: Added more tests for helper lists

--- app/test/freeRTOS/helpers/test_list/src/tests.cpp
+++ app/test/freeRTOS/helpers/test_list/src/tests.cpp
@@ -65,6 +65,35 @@ void test_slist_insert_erase_after()
   list.clear();
 }
 
+void test_slist_copy_swap_reverse_max_size()
+{
+  SList<int> list;
+  SNode<int> n1(1);
+  SNode<int> n2(2);
+  SNode<int> n3(3);
+
+  list.push_front(&n1); // [1]
+  list.push_front(&n2); // [2, 1]
+  list.push_front(&n3); // [3, 2, 1]
+
+  TEST_ASSERT_TRUE(list.max_size() > 0);
+
+  // Shallow copy shares nodes by design in this intrusive list.
+  SList<int> copy = list;
+  TEST_ASSERT_EQUAL(3, copy.front());
+
+  list.reverse(); // [1, 2, 3]
+  TEST_ASSERT_EQUAL(1, list.front());
+
+  SList<int> other;
+  SNode<int> n4(4);
+  other.push_front(&n4);
+  list.swap(other);
+
+  TEST_ASSERT_EQUAL(4, list.front());
+  TEST_ASSERT_EQUAL(1, other.front());
+}
+
 // --- DTailList Tests (Doubly-linked list with tail pointer) ---
 
 void test_dtaillist_push_pop()
@@ -133,6 +162,57 @@ void test_dtaillist_iterator()
   list.clear();
 }
 
+void test_dtaillist_end_decrement_and_reverse_iterators()
+{
+  DTailList<int> list;
+  DNode<int> n1(1);
+  DNode<int> n2(2);
+  DNode<int> n3(3);
+
+  list.push_back(&n1);
+  list.push_back(&n2);
+  list.push_back(&n3);
+
+  auto it = list.end();
+  --it;
+  TEST_ASSERT_EQUAL(3, *it);
+
+  int expected = 3;
+  for (auto rit = list.rbegin(); rit != list.rend(); ++rit) {
+    TEST_ASSERT_EQUAL(expected--, *rit);
+  }
+  TEST_ASSERT_EQUAL(0, expected);
+
+  auto cit = list.cend();
+  --cit;
+  TEST_ASSERT_EQUAL(3, *cit);
+
+  TEST_ASSERT_TRUE(list.max_size() > 0);
+}
+
+void test_dtaillist_swap_and_reverse()
+{
+  DTailList<int> list;
+  DNode<int> n1(1);
+  DNode<int> n2(2);
+  DNode<int> n3(3);
+  list.push_back(&n1);
+  list.push_back(&n2);
+  list.push_back(&n3);
+
+  DTailList<int> other;
+  DNode<int> n4(4);
+  other.push_back(&n4);
+
+  list.swap(other);
+  TEST_ASSERT_EQUAL(4, list.front());
+  TEST_ASSERT_EQUAL(1, other.front());
+
+  other.reverse(); // [3, 2, 1]
+  TEST_ASSERT_EQUAL(3, other.front());
+  TEST_ASSERT_EQUAL(1, other.back());
+}
+
 // --- STailList Tests (Singly-linked list with tail pointer) ---
 
 void test_staillist_push_pop()
@@ -155,6 +235,35 @@ void test_staillist_push_pop()
   TEST_ASSERT_EQUAL(200, popped->data);
 
   TEST_ASSERT_TRUE(list.empty());
+}
+
+void test_staillist_swap_reverse_max_size_and_insert_after_const()
+{
+  STailList<int> list;
+  SNode<int> n1(1);
+  SNode<int> n2(2);
+  SNode<int> n3(3);
+  list.push_back(&n1);
+  list.push_back(&n3); // [1, 3]
+
+  STailList<int>::const_iterator pos = list.begin();
+  list.insert_after(pos, &n2); // [1, 2, 3]
+
+  TEST_ASSERT_EQUAL(1, list.front());
+  TEST_ASSERT_EQUAL(3, list.back());
+
+  list.reverse(); // [3, 2, 1]
+  TEST_ASSERT_EQUAL(3, list.front());
+  TEST_ASSERT_EQUAL(1, list.back());
+
+  STailList<int> other;
+  SNode<int> n4(4);
+  other.push_back(&n4);
+  list.swap(other);
+
+  TEST_ASSERT_EQUAL(4, list.front());
+  TEST_ASSERT_EQUAL(3, other.front());
+  TEST_ASSERT_TRUE(list.max_size() > 0);
 }
 
 // --- DList Tests (Doubly-linked list without tail pointer) ---
@@ -203,20 +312,75 @@ void test_dlist_insert_erase()
   TEST_ASSERT_EQUAL(1, erased->data);
 }
 
+void test_dlist_end_decrement_and_reverse_iterators()
+{
+  DList<int> list;
+  DNode<int> n1(1);
+  DNode<int> n2(2);
+  DNode<int> n3(3);
+  list.push_back(&n1);
+  list.push_back(&n2);
+  list.push_back(&n3);
+
+  auto it = list.end();
+  --it;
+  TEST_ASSERT_EQUAL(3, *it);
+
+  int expected = 3;
+  for (auto rit = list.rbegin(); rit != list.rend(); ++rit) {
+    TEST_ASSERT_EQUAL(expected--, *rit);
+  }
+  TEST_ASSERT_EQUAL(0, expected);
+
+  auto cit = list.cend();
+  --cit;
+  TEST_ASSERT_EQUAL(3, *cit);
+}
+
+void test_dlist_swap_reverse_and_max_size()
+{
+  DList<int> list;
+  DNode<int> n1(1);
+  DNode<int> n2(2);
+  DNode<int> n3(3);
+  list.push_back(&n1);
+  list.push_back(&n2);
+  list.push_back(&n3);
+
+  DList<int> other;
+  DNode<int> n4(4);
+  other.push_back(&n4);
+
+  list.swap(other);
+  TEST_ASSERT_EQUAL(4, list.front());
+  TEST_ASSERT_EQUAL(1, other.front());
+
+  other.reverse(); // [3, 2, 1]
+  TEST_ASSERT_EQUAL(3, other.front());
+  TEST_ASSERT_EQUAL(1, other.back());
+  TEST_ASSERT_TRUE(other.max_size() > 0);
+}
+
 void runUnityTests()
 {
   UNITY_BEGIN();
 
   RUN_TEST(test_slist_push_pop_front);
   RUN_TEST(test_slist_insert_erase_after);
+  RUN_TEST(test_slist_copy_swap_reverse_max_size);
 
   RUN_TEST(test_dtaillist_push_pop);
   RUN_TEST(test_dtaillist_iterator);
+  RUN_TEST(test_dtaillist_end_decrement_and_reverse_iterators);
+  RUN_TEST(test_dtaillist_swap_and_reverse);
 
   RUN_TEST(test_staillist_push_pop);
+  RUN_TEST(test_staillist_swap_reverse_max_size_and_insert_after_const);
 
   RUN_TEST(test_dlist_push_pop);
   RUN_TEST(test_dlist_insert_erase);
+  RUN_TEST(test_dlist_end_decrement_and_reverse_iterators);
+  RUN_TEST(test_dlist_swap_reverse_and_max_size);
 
   UNITY_END();
 }

24da54449b40e0fe6816b439b42dfffc0565a963
Alessandro Mauri <alemauri001@gmail.com>
2026-05-09 12:21:51 +0200

    tweaked lora packet format

--- examples/logger/include/lora.h
+++ examples/logger/include/lora.h
@@ -3,29 +3,38 @@
 #include <Arduino.h>
 #include <float16.h>
 
+
+enum LoRaPayloadType : uint8_t {
+	PAYLOAD_1,
+	PAYLOAD_2,
+	PAYLOAD_3,
+	PAYLOAD_STR
+};
+
 //payload da trasmettere
+// TODO: utlizza meglio lo spazio
 typedef union {
-	struct { //dimensioni da capire (così si trasmette 40 pacchetti al secondo)
-		float16 AccX;
-		float16 AccY;
-		float16 AccZ;
-		float16 GyroX;
-		float16 GyroY;
-		float16 GyroZ;
-		float16 EulerPitch;
-		float16 EulerRoll;
-		float16 EulerYaw;
-		float16 AccX1;
-		float16 AccY1;
-		float16 AccZ1;
-		float16 GyroX1;
-		float16 GyroY1;
-		float16 GyroZ1;
-		float16 EulerPitch1;
-		float16 EulerRoll1;
-		float16 EulerYaw1;
-	} values;
-	uint8_t bytes[sizeof(values)];
+	struct {
+		uint64_t timestamp;
+		LoRaPayloadType type;
+		union {
+			struct {
+				float16 roll, pitch, yaw;
+				float16 vert_acc;
+				float16 airbrake_angle;
+			} p1;
+			struct {
+				float16 temp1, temp2, temp3;
+				uint8_t para_state;
+			} p2;
+			struct {
+				float16 pos_dx, pos_dy, pos_dz;
+			} p3;
+			char str[10];
+		};
+		uint8_t err_flag;
+	} data;
+	uint8_t bytes[sizeof(data)];
 } LoRaPayload;
 
 

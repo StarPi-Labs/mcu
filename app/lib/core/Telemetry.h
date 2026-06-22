cd0e7f6b56e266059146c12bd158b949e4867ab7
AimanDebug <105602441+AimanDebug@users.noreply.github.com>
2026-06-22 20:23:02 +0200

    feat: added constexpr to telemetry update functions, minor comment updates

--- app/lib/core/Telemetry.h
+++ app/lib/core/Telemetry.h
@@ -38,11 +38,14 @@ enum class FlightStatus {
 
 /// @brief Represents the status of the LoRa link for telemetry purposes.
 struct LoRaLinkStatus {
-  bool connected; // Whether the LoRa link is currently established
-  float rssi;     // Received Signal Strength Indicator in dBm
-  float snr;      // Signal-to-Noise Ratio in dB
+  bool connected; //< Whether the LoRa link is currently established
+  float rssi;     //< Received Signal Strength Indicator in dBm
+  float snr;      //< Signal-to-Noise Ratio in dB
 };
 
+/// @brief A concept that defines the required interface for a telemetry
+/// callback. Any class that wants to receive telemetry updates must implement
+/// this interface.
 template <typename T>
 concept ManagerCallback = requires(T t) {
   requires requires(FlightStatus status) {
@@ -129,13 +132,13 @@ public:
   ///
   /// @param state A @c State enum value representing the current state of the
   /// rocket.
-  void updateFlightStatus(FlightStatus status);
+  constexpr void updateFlightStatus(FlightStatus status);
 
   /// @brief Updates the link status for telemetry transmission.
   ///
   /// @param status A @c LoRaLinkStatus struct representing the current link
   /// status
-  void updateLinkStatus(const LoRaLinkStatus& status);
+  constexpr void updateLinkStatus(const LoRaLinkStatus& status);
 
   /// @brief Updates the rotation axes (roll, pitch, yaw) for telemetry
   /// transmission.
@@ -143,14 +146,14 @@ public:
   /// @param roll Roll angle in degrees.
   /// @param pitch Pitch angle in degrees.
   /// @param yaw Yaw angle in degrees.
-  void updateAttitude(float roll, float pitch, float yaw);
+  constexpr void updateAttitude(float roll, float pitch, float yaw);
 
   /// @brief Updates the position (latitude, longitude) for telemetry
   /// transmission.
   ///
   /// @param latitude Latitude in degrees.
   /// @param longitude Longitude in degrees.
-  void updateMapPosition(float latitude, float longitude);
+  constexpr void updateMapPosition(float latitude, float longitude);
 
   /// @brief Updates the acceleration values for telemetry transmission.
   ///
@@ -160,36 +163,36 @@ public:
   /// @param gx Angular velocity around the x-axis in degrees per second.
   /// @param gy Angular velocity around the y-axis in degrees per second.
   /// @param gz Angular velocity around the z-axis in degrees per second.
-  void updateAcceleration(float ax, float ay, float az, float gx, float gy,
-                          float gz);
+  constexpr void updateAcceleration(float ax, float ay, float az, float gx,
+                                    float gy, float gz);
 
   /// @brief Updates the altitude for telemetry transmission.
   ///
   /// @param altitude Altitude in meters.
-  void updateAltitude(float altitude);
+  constexpr void updateAltitude(float altitude);
 
   /// @brief Updates the vertical velocity for telemetry transmission.
   ///
   /// @param verticalVelocity Vertical velocity in meters per second.
-  void updateVerticalVelocity(float verticalVelocity);
+  constexpr void updateVerticalVelocity(float verticalVelocity);
 
   /// @brief Updates the pressure readings from two barometers for telemetry
   /// transmission.
   ///
   /// @param pressure1 Pressure reading from the first barometer in mBar.
   /// @param pressure2 Pressure reading from the second barometer in mBar.
-  void updatePressure(float pressure1, float pressure2);
+  constexpr void updatePressure(float pressure1, float pressure2);
 
   /// @brief Updates the temperature readings for telemetry transmission.
   ///
   /// @param temperature1 Temperature reading from the first sensor in degrees.
   /// @param temperature2 Temperature reading from the second sensor in degrees.
-  void updateTemperature(float temperature1, float temperature2);
+  constexpr void updateTemperature(float temperature1, float temperature2);
 
   /// @brief Updates the airbrake extension
   ///
   /// @param extension Airbrake extension percentage [0.0, 1.0]
-  void updateAirbrakeExtension(float extension);
+  constexpr void updateAirbrakeExtension(float extension);
 
 private:
   std::tuple<Callbacks...> m_callbacks;
